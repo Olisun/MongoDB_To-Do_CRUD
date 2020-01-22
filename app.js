71441a239d8a5ae4becd5c30589785a8777b1933
Oliver Sun <olisun@mac.com>
2020-01-22 14:41:02 -0800

    added code to serve the static HTML file and to get data from the db which is blank.

--- app.js
+++ app.js
@@ -6,7 +6,24 @@ const collection = "todo";
 const app = express();
 app.use(bodyParser.json());
 
-db.connect((error) => {
+// For serving the static HTML file.
+app.get("/", (req, res) => {
+  res.sendFile(path.join(_dirname, "index.html"));
+});
+
+app.get("/getTodos", (req, res) => {
+  db.getDB().collection(collection).find({}).toArray((error, documents) => {
+    if (error) {
+      console.log("error");
+    } else {
+      console.log("documents");
+      res.json(documents);
+    }
+  })
+})
+
+// For the server. 
+db.connectToMongo((error) => {
   if (error) {
     console.log("unable to connected to database");
     process.exit(1);
